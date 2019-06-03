d3d6456a7f6c0f8dc185d519fab8373b7885b180
HEE SEOK SHIN <4sizn@naver.com>
2019-06-03 13:52:09 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -16,14 +16,23 @@ export const METHOD = {
 const VS = ["이김", "짐", "비김"] // 0, 1, 2
 
 const makePlayer = hand => {
+  assert(hand >= 0 && hand < 3, "player hands problem...")
   const player = {}
   player.hand = hand
   return player
 }
 
 const rpsLogic = (p1, p2) => {
+  console.log(
+    `너는 ${METHOD[p1.hand]}이고, 상대는 ${METHOD[p2.hand]}이며 넌[${
+      VS[(p1.hand + p2.hand * -1 + 2) % 3]
+    }]`
+  )
   return (p1.hand + p2.hand * -1 + 2) % 3
 }
+
+// 전체 인원에 대한 세부 결과값이 나와야 한다.
+// [player:hand, player:r='w', player:hand, player:r='l']
 
 const singleGame = (me, every) => {
   return every.map(p => rpsLogic(me, p))
@@ -39,14 +48,18 @@ const makeComputers = n => {
 const rpsSimulation = hands =>
   hands.map(h => singleGame(h, [h, ...makeComputers(WEB2_MAN_NUM - 1)]))
 
-// 전체 인원에 대한 세부 결과값이 나와야 한다.
-// [player:hand, player:r='w', player:hand, player:r='l']
-
 const dailyQueue = makeComputers
+
+function assert(condition, message) {
+  if (!condition) {
+    throw new Error(message || "Assertion failed")
+  }
+}
 
 module.exports = {
   METHOD,
   VS,
+  rpsLogic,
   dailyQueue,
   makePlayer,
   makeComputers,
